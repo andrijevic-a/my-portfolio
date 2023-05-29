5f3d0d9ca46dc62e2bd147b9bd29cb338a262455
andrijevic-a <aleksandarandrijevic@gmail.com>
2023-05-29 16:31:39 +0200

    Finished mobile responsivness

--- main.js
+++ main.js
@@ -1,35 +1,45 @@
 const handleScrollAnimation = (groups, windowHeight, scrollPosition) => {
-  groups.forEach(function(group) {
+  groups.forEach(function (group) {
     const groupTop = group.offsetTop;
     const groupHeight = group.offsetHeight;
 
-    if (scrollPosition > groupTop - windowHeight + groupHeight && scrollPosition < groupTop + groupHeight) {
+    if (
+      scrollPosition > groupTop - windowHeight + groupHeight &&
+      scrollPosition < groupTop + groupHeight
+    ) {
       group.style.opacity = '1';
     } else {
       group.style.opacity = '0';
     }
   });
-}
+};
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
   const leftSideSection = document.querySelector('.left-side');
   const leftSideGroups = leftSideSection.querySelectorAll('.container-l');
-  
+  const leftSideRGroups = leftSideSection.querySelectorAll('.container-r');
+
   const rightSideSection = document.querySelector('.right-side');
   const rightSideGroups = rightSideSection.querySelectorAll('.container-r');
 
   const centerSection = document.querySelector('.about-me-section');
   const centerGroups = centerSection.querySelectorAll('.text-container');
-  
+
   const windowHeight = window.innerHeight;
 
   const scrollHandler = () => {
     const scrollPosition = window.scrollY || window.pageYOffset;
-    
-    handleScrollAnimation(leftSideGroups, windowHeight, scrollPosition);
-    handleScrollAnimation(rightSideGroups, windowHeight, scrollPosition);
-    handleScrollAnimation(centerGroups, windowHeight, scrollPosition);
-  }
+
+    if (window.innerWidth > 768) {
+      handleScrollAnimation(leftSideGroups, windowHeight, scrollPosition);
+      handleScrollAnimation(rightSideGroups, windowHeight, scrollPosition);
+      handleScrollAnimation(centerGroups, windowHeight, scrollPosition);
+    } else {
+      handleScrollAnimation(leftSideGroups, windowHeight, scrollPosition);
+      handleScrollAnimation(leftSideRGroups, windowHeight, scrollPosition);
+      handleScrollAnimation(centerGroups, windowHeight, scrollPosition);
+    }
+  };
 
   window.addEventListener('scroll', scrollHandler);
   scrollHandler(); // Trigger the initial animation state
